3c1ee2bb0f7b786406427b1330aeb2efaad0f7d3
mallappwaghamore-png <mallappwaghamore@gmail.com>
2026-06-28 19:56:38 +0530

    Time: 20 ms (11.06%), Space: 30.5 MB (41.53%) - LeetHub

---
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        left=0
+        answer=float("inf")
+        current_sum=0
+        for right in range(len(nums)):
+            current_sum+=nums[right]
+
+            while current_sum>=target:
+                answer=min(answer,right-left+1)
+
+                current_sum-=nums[left]
+                left+=1
+        if answer==float("inf"):
+            return 0
+        return answer
