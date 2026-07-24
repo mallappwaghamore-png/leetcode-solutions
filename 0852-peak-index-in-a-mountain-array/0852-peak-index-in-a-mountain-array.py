f935bf653503f3582816492b5883ae2142f9c1f4
mallappwaghamore-png <mallappwaghamore@gmail.com>
2026-07-24 19:11:44 +0530

    Time: 0 ms (100%), Space: 31.4 MB (47.83%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -4,16 +4,15 @@ class Solution:
         right=len(arr)-1
         while left<right:
             mid=(left+right)//2
-
             if arr[mid]<arr[mid+1]:
                 left=mid+1
-            elif arr[mid]>arr[mid+1]:
+            else:
                 right=mid
         return left
 
                 
             
             
-        return mid
+        
                 
             
